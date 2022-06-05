f4f7cdc0c59390fce2c4a01e3812a5f46433b9ed
kathryncodes <katnicjohn@gmail.com>
2022-06-04 19:26:48 -0700

    spotify-hits-api v1

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const fs = require('fs');
+
+const jsonData = fs.readFileSync('../spotify-hits-api/data.json', "utf8", (err, jsonString) => {
+    if(err){
+        console.log("error");
+        return;
+    }
+   return jsonData;
+})
+
+const dataObj = JSON.parse(jsonData);
+const PORT = 8000;
+
+app.get('/',  (request, response) => {
+    response.sendFile(__dirname + '/index.html');
+
+})
+
+app.get('/api',(request, response) => {
+    response.sendFile(__dirname + '/data.json');
+})
+
+app.get('/api/:year', (request, response) => {
+    const year = request.params.year;
+    response.send(dataObj.data[year]);
+
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log("The server is running")
+})
+
